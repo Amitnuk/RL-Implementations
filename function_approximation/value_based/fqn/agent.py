f5907376ebca8a24be251fea2fbb3ed856a20006
kuntimakiala <kuntimakiala@github.com>
2026-06-27 19:09:48 +0200

    Agent Initialization

--- function_approximation/value_based/fqn/agent.py
+++ function_approximation/value_based/fqn/agent.py
@@ -3,8 +3,8 @@ from torch import nn
 from torch import optim
 from torch.nn import MSELoss
 import numpy as np 
-
-
+import gymnasium as gym 
+from gymnasium.spaces import Box, Discrete
 
 
 
@@ -16,6 +16,59 @@ class FittedQNet() :
 
 
 class FittedAgent :
-    pass
+    def __init__(self,
+                 env_name:str,
+                 value_model_fn:nn.Module,
+                 value_optimizer_fn,
+                 value_optimizer_lr:float,
+                 training_strategy_fn) :
+        
+        self.env_name = env_name
+                
+        self.Env = gym.make(id=self.env_name,render_mode="human") 
+        _,_ = self.Env.reset()
 
 
+        if isinstance(self.Env.action_space,Box) :
+            self.action_dim = self.Env.action_space.shape[0]
+        elif isinstance(self.Env.action_space,Discrete) :
+            self.action_dim =self.Env.action_space.n
+        else:
+            raise NotImplementedError(f"Unsupported action space: {type(self.Env.action_space)}")
+
+        if isinstance(self.Env.observation_space,Box) :
+            self.observation_dim = self.Env.observation_space.shape[0]
+        elif isinstance(self.Env.observation_space,Discrete) :
+            self.observation_dim =self.Env.observation_space.n
+        else:
+            raise NotImplementedError(f"Unsupported onservation space: {type(self.Env.observation_space)}")
+        
+        self.nS, self.nA = self.observation_dim, self.action_dim
+        print(f"Selected env = {self.env_name}\nnumber of states S = {self.nS}\nnumber of actions A = {self.nA}")
+
+        #self.online_model = value_model_fn(self.nS, self.nA)
+
+
+        self.value_model_fn       = value_model_fn
+        self.value_optimizer_fn   = value_optimizer_fn
+        self.value_optimizer_lr   = value_optimizer_lr 
+        self.training_strategy_fn = training_strategy_fn
+        
+        #self.evaluation_strategy_fn= evaluation_strategy_fn
+        #self.batch_size=batch_size
+
+        self.cummulative_reward_per_episdode = 0
+    def interact(self):
+
+        while True :
+            
+            action = self.Env.action_space.sample()
+            
+
+            state, reward, terminal, truncated, info = self.Env.step(action=action)
+            self.cummulative_reward_per_episdode += reward
+            
+            if truncated or terminal :
+                break
+
+        print(f"Cummulative = reward : {self.cummulative_reward_per_episdode}")   
--- tests/test_fqn.py
+++ tests/test_fqn.py
@@ -1,8 +1,13 @@
 import torch
+from torch.optim import Adam, RMSprop, SGD
 import numpy as np
+from torch.nn import MSELoss
 from function_approximation.value_based.fqn.network import FCQNetwork
+from function_approximation.value_based.fqn.agent import FittedAgent
+
 from common.policies.epsilon_greedy import EpsilonGreedyStrategy
 from common.policies.softmax import SoftMaxStrategy
+import gymnasium as gym
 import argparse
 
 if __name__ == "__main__" :
@@ -13,6 +18,7 @@ if __name__ == "__main__" :
 
     parser.add_argument("-e","--exp_strategy",default=0,type=int,choices=[0,1],help="Exploration strategy, Softmax -> 0 is default")
     parser.add_argument("--use_gpu",action="store_true",help="Use GPU")
+    parser.add_argument("--optimizer",default=0,type=int,choices=[0,1,2],help="The optimizer to select, 0 for Adam, 1 for RMSProp and 2 for SGD, Adam is default")
     args = parser.parse_args()
 
     
@@ -38,4 +44,23 @@ if __name__ == "__main__" :
 
     action = Strategy.select_action(model=QNet,state=state)
     print(f"action={action, type(action)}")
+
+
+    print(f"{gym.pprint_registry()}")
+
+    model = lambda nS, nA : FCQNetwork(input_shape=nS, output_shape=nA,hidden_units=(256,256),device=device)
+
+ 
+    optimizer = lambda model, lr :Adam(params=model.parameter(), lr=lr) if args.optimizer == 0 else RMSprop(params=model.parameter(), lr=lr) if args.optimizer == 1 else optim.optim.SGD(params=model.parameter(), lr=lr)
+
+    env_name ="MountainCarContinuous-v0"
+    env = gym.make(id=env_name)
     
+
+    Agent = FittedAgent(env_name=env_name,
+                        value_model_fn=model,
+                        value_optimizer_fn=optimizer,
+                        value_optimizer_lr=0.01,
+                        training_strategy_fn=Strategy
+                        )
+    #Agent.interact()
